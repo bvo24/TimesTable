847ba443650a30a8bed43cb2ca08c289b50e7bbc
bvo24 <brianvo2020@gmail.com>
2024-05-01 17:57:34 -0400

    Initial Commit

---
+++ TimesTable/TimesTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTableApp.swift
+//  TimesTable
+//
+//  Created by Brian Vo on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TimesTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
